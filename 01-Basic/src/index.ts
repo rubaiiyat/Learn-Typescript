e5ad8d990eea3861e52bdad56867b23448747edf
rubaiiyat <abirr1516@gmail.com>
2025-11-04 18:17:20 +0600

    basic ts

---
+++ 01-Basic/src/index.ts
@@ -0,0 +1,11 @@
+function great(name: string) {
+  return `Hello ${name}`;
+}
+
+console.log(great("Rubaiyat"));
+
+let name = "Abir";
+name = "Rubaiyat";
+let age = 20;
+console.log(name);
+console.log(age);
