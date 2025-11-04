01e584e47d4931fcb05c24f19118526a2f1330e6
rubaiiyat <abirr1516@gmail.com>
2025-11-04 19:02:09 +0600

    ts arr

---
+++ 01-Basic/src/arr.ts
@@ -0,0 +1,10 @@
+const names: string[] = [];
+
+names.push("Bob");
+names.push("John");
+names.push("Lennon");
+
+const numbers: number[] = [10, 20, 30, 40, 50];
+
+names.map((name) => console.log(name));
+numbers.map((number) => console.log(number));
